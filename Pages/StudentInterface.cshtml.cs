5f6f40aa9e228d0e093249a400790caed2f6a0be
Muhammad Ibrahim Basit <ibasit050802@gmail.com>
2023-04-29 18:47:38 +0500

    fxed facultu pop p modal sheet,added new page and ran for further tests

---
+++ Pages/StudentInterface.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FLEXX.Pages
+{
+    public class StudentInterfaceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
