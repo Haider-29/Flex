0346eab35da252aac44ce880f46232a7a5f38440
Muhammad Ibrahim Basit <ibasit050802@gmail.com>
2023-05-04 01:10:09 +0500

    Fix: Passing List of Values from html form to post function, OnGetAsync losing its email, password values after post function is called

--- Pages/FacultyInterface.cshtml.cs
+++ Pages/FacultyInterface.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 namespace FLEXX.Pages
 {
@@ -18,7 +19,28 @@ namespace FLEXX.Pages
 
     }
 
+    public class Student
+    {
+        public string? RollNum { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? section { get; set; }
+
+    }
+
+
+    public class studentAttendance {
     
+        public string? RollNum { get; set;}
+        public string? Name { get; set;}
+        public string? Section { get; set;}
+        public DateTime? Date { get; set;}
+        public string? Status { get; set;}
+    }
+
+
+
 
     public class FacultyInterfaceModel : PageModel
     {
@@ -30,16 +52,196 @@ namespace FLEXX.Pages
             _configuration = configuration;
         }
 
+        [BindProperty]
+
+        public string Message { get; set; }
+
         public List<MarksDistributionTable> AssignedSections { get; set; }
 
-
+        public List<Student> Students { get; set; }
         [BindProperty]
         public string TeacherEmail { get; set; }
 
         [BindProperty]
         public string TeacherPassword { get; set; }
 
-        
+        [BindProperty]
+
+        public string NSectionID { get; set; }
+
+        [BindProperty]
+        public string NEvaluationType { get; set; }
+
+        [BindProperty]
+
+        public Int32 NMaxMarks { get; set; }
+
+        [BindProperty]
+
+        public Int32 NWeightage { get; set; }
+
+
+
+        [BindProperty]
+
+        public List<studentAttendance> Attendances { get; set; }    = new List<studentAttendance>();
+
+        public async Task<IActionResult> OnPostAddEvaluationAsync()
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString)) { 
+            
+                await connection.OpenAsync();
+
+                // attendance ids
+                string q = "SELECT TOP 1 EvaluationID FROM Evaluation ORDER BY EvaluationID DESC";
+                SqlCommand cmd = new SqlCommand(q, connection);
+
+                SqlDataReader reader = await cmd.ExecuteReaderAsync();
+                int evaluationID = 1;
+
+                if (reader.HasRows)
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        evaluationID = reader.GetInt32(0) + 1;
+                        break;
+                    }
+                }
+
+                reader.Close();
+                cmd.Dispose();
+
+                string query = "Insert into Evaluation (EvaluationID, SectionID, EvaluationType, Weightage, MaxMarks) " +
+                    "VALUES (@EvaluationID, @NSectionID, @NEvaluationType, @NWeightage, @NMaxMarks);";
+                SqlCommand insertEvaluation = new SqlCommand(query, connection);
+                insertEvaluation.Parameters.AddWithValue("@NsectionID", NSectionID);
+                insertEvaluation.Parameters.AddWithValue("@NEvaluationType", NEvaluationType);
+                insertEvaluation.Parameters.AddWithValue("@NWeightage", NWeightage);
+                insertEvaluation.Parameters.AddWithValue("@NMaxMarks", NMaxMarks);
+                insertEvaluation.Parameters.AddWithValue("@EvaluationID", evaluationID);
+                try
+                {
+
+                    int rowsEffected = await insertEvaluation.ExecuteNonQueryAsync();
+                    insertEvaluation.Dispose();
+                    connection.Close();
+                    if (rowsEffected > 0)
+                    {
+                        Message = "Evaluation Added!";
+                        await OnGetAsync(TeacherEmail, TeacherPassword);
+                        return Page();
+                    }
+                } catch(SqlException sqlEx)
+                {
+                    insertEvaluation.Dispose();
+                    connection.Close();
+
+                    Message = "An error occured while adding evaluation";
+                    await OnGetAsync(TeacherEmail, TeacherPassword);
+                    return Page();
+                }
+
+                await OnGetAsync(TeacherEmail, TeacherPassword);
+                return Page();
+
+            }
+        }
+
+        public async Task<IActionResult> OnPostAddAttendanceAsync(string[] studentIds, string sectionId, DateTime date, string[] statuses)
+        {
+
+
+            Console.Write("Section: ", sectionId);
+            Console.Write("Date: ", date);
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                // attendance ids
+                string q = "SELECT TOP 1 AttendanceID FROM Attendance ORDER BY AttendanceID DESC";
+                SqlCommand cmd = new SqlCommand(q, connection);
+
+                SqlDataReader reader = await cmd.ExecuteReaderAsync();
+                int attendanceID = 1;
+
+                if (reader.HasRows)
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        attendanceID = reader.GetInt32(0) + 1;
+                        break;
+                    }
+                }
+
+                reader.Close();
+                cmd.Dispose();
+
+                // insert attendance records
+                string query = "INSERT INTO Attendance (AttendanceID, StudentID, SectionID, Date, Status) " +
+                "VALUES (@AttendanceID, @StudentID, @SectionID, @Date, @Status)";
+                SqlCommand cmd2 = new SqlCommand(query, connection);
+
+                for (int i = 0; i < studentIds.Length; i++)
+                {
+                    cmd2.Parameters.Clear();
+                    cmd2.Parameters.AddWithValue("@AttendanceID", attendanceID);
+                    cmd2.Parameters.AddWithValue("@StudentID", studentIds[i]);
+                    cmd2.Parameters.AddWithValue("@SectionID", sectionId);
+                    cmd2.Parameters.AddWithValue("@Date", date);
+                    cmd2.Parameters.AddWithValue("@Status", statuses[i]);
+
+                    try
+                    {
+                        int rowsAffected = await cmd2.ExecuteNonQueryAsync();
+
+                        cmd2.Dispose();
+                        connection.Close();
+
+                        if (rowsAffected > 0)
+                        {
+                            // Insertion was successful
+                            Message = "Attendance adding successful!";
+                            await OnGetAsync(TeacherEmail, TeacherPassword);
+                            return Page(); // Redirect to login page or any other page you'd like
+                        }
+                        else
+                        {
+                            // Insertion failed
+                            Message = "Attendance Adding Failed";
+                            await OnGetAsync(TeacherEmail, TeacherPassword);
+                            return Page(); // Stay on the same page and show the error message
+                        }
+                    }
+                    catch (SqlException ex)
+                    {
+                        cmd2.Dispose();
+                        connection.Close();
+
+                        if (ex.Number == 2627)
+                        {
+                            // Primary key violation (duplicate email)
+                            Message = "The course already exists";
+                        }
+                        else
+                        {
+                            // Other SQL errors
+                            Message = "An error occurred during registration. Please try again.";
+                        }
+
+                        await OnGetAsync(TeacherEmail, TeacherPassword);
+                        return Page(); // Stay on the same page and show the error message
+                    }
+
+                }
+            }
+            await OnGetAsync(TeacherEmail, TeacherPassword);
+            return Page();
+        }
+
+
+
 
         public async Task OnGetAsync(string email, string password)
         {
@@ -73,7 +275,7 @@ namespace FLEXX.Pages
                     SqlDataReader reader2 = await cmd2.ExecuteReaderAsync();
                     string teacher_name = "";
                     if (reader2.Read()) {
-                         teacher_name = reader2.GetString(0);
+                        teacher_name = reader2.GetString(0);
                         teacher_name += " ";
                         teacher_name += reader2.GetString(1);
                     }
@@ -117,6 +319,25 @@ namespace FLEXX.Pages
                 reader.Close();
                 cmd.Dispose();
 
+                string q2 = "SELECT u.Username, u.FName, u.LName, ss.sectionid FROM student_section AS ss JOIN users AS u ON ss.STUDENTID = u.Username WHERE u.Role = 'S';";
+                SqlCommand cum = new SqlCommand(q2, connection);
+                SqlDataReader readStudents = await cum.ExecuteReaderAsync();
+                Students = new List<Student>();
+                while (await  readStudents.ReadAsync())
+                {
+                    Student student = new Student()
+                    {
+                        Name = readStudents.GetString(1) + " " + readStudents.GetString(2),
+                        RollNum = readStudents.GetString(0),
+                        // section soch bsdk
+                        section = readStudents.GetString(2)
+
+                    };
+                    Students.Add(student);
+                }
+                // get students of a sections
+                readStudents.Close();
+                cum.Dispose();
                 connection.Close();
             }
 
@@ -125,5 +346,4 @@ namespace FLEXX.Pages
     }
 }
 
-// ibrahim was here
-// another run after running
+
