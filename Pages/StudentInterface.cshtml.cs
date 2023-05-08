1e1b110ca7c4bb7d82d7f9aa0bb65310506e1081
Muhammad Ibrahim Basit <ibasit050802@gmail.com>
2023-05-08 22:31:59 +0500

    cgpa calculation

--- Pages/StudentInterface.cshtml.cs
+++ Pages/StudentInterface.cshtml.cs
@@ -39,6 +39,27 @@ namespace FLEXX.Pages
 
             public string Semester { get; set; }
 
+            public double GetGradePoints()
+            {
+                var gradePointsLookup = new Dictionary<string, double>
+        {
+            {"A+", 4.0},
+            {"A", 3.7},
+            {"A-", 3.3},
+            {"B+", 3.0},
+            {"B", 2.7},
+            {"B-", 2.3},
+            {"C+", 2.0},
+            {"C", 1.7},
+            {"C-", 1.3},
+            {"D+", 1.0},
+            {"D", 0.7},
+            {"F", 0}
+        };
+
+                return gradePointsLookup.TryGetValue(Grade, out var gradePoints) ? gradePoints : 0;
+            }
+
         }
 
         public class Attendance {
